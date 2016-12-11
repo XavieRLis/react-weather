a72e9b860a10a9686756651dbe9ca690da24595a
xavier.lis <xavier.lis@gmail.com>
2016-12-11 20:55:40 +0300

    Style Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => (
-  <h3>Examples</h3>
+  <div>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are few example locations to try out</p>
+    <ol>
+      <li>
+        <Link to='/?location=London'>London</Link>
+      </li>
+      <li>
+        <Link to='/?location=Moskow'>Moskow</Link>
+      </li>
+    </ol>
+  </div>
 );
 
 module.exports = Examples;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,8 +4,11 @@ var Navigation = require('Navigation');
 var Main = (props) => (
   <div>
     <Navigation />
-    <h2>Main Component</h2>
-    {props.children}
+    <div className='row'>
+      <div className="columns medium-6 large-4 small-centered">
+        {props.children}
+      </div>
+    </div>
   </div>
 );
 
